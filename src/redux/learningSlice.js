6d3f91f16cd2bc88b9481745e6d96e7b244ff881
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-05-09 19:00:55 +1000

    Milestone 2: Created learningSlice to manage current and finished drug lists

---
+++ src/redux/learningSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+export const learningSlice = createSlice({
+    name: "Learning",
+    initialState: {
+        current : [],
+        finished: [],
+    },
+    reducers: {
+        addToLearning: (state, action) =>{
+            const drug = action.payload;
+            if(!state.current.find(d => d.id === drug.id)) {
+                state.current.push(drug);
+            }
+        },
+        finishDrug: (state, action) => {
+            const drugId = action.payload;
+            const drug = state.current.find(d => d.id === drug.id);
+            if (drug) {
+                state.current = state.current.filter(d => d.id !== drugId);
+                state.finished.push(drug);
+            }
+        },
+        removeDrug: (state, action) => {
+            const id = action.payload;
+            state.current = state.current.filter(d => d.id !== id);
+        },
+    },
+});
+
+export const {addToLearning, finishDrug, removeDrug} = learningSlice.actions;
+export default learningSlice.reducer;
