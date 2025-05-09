97d2a792e8bc5f73cca360988762cf466313487f
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-05-09 18:53:51 +1000

    Milestone 2: Added learning list logic: hide Study button and gray out drugs in the learning list.

--- src/screens/DrugInCategoryScreen.js
+++ src/screens/DrugInCategoryScreen.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Text, View, FlatList, StyleSheet} from 'react-native';
 import DrugItem from '../components/DrugItem';
 import {drugData} from '../../resources/resource';
+import { useSelector } from 'react-redux';
 
 export default function DrugInCategoryScreen({ route, navigation }) {
 
   const { category} = route.params;
+
+  const learningList = useSelector(state => state.learning.current);
   
   const drugs = drugData.filter(drug => drug.categories.includes(category.id));
 
@@ -15,20 +18,24 @@ export default function DrugInCategoryScreen({ route, navigation }) {
       <FlatList
         data={drugs}
         keyExtractor={(item) =>item.id.toString()}
-        renderItem={({item})=>(
-          <DrugItem
-            name={item.name}
-            onPress={() => navigation.navigate('Drug Details', {drug: item})}
-          />
-        )}
-      />
+        renderItem={({item})=>{
+          const isLearning = learningList.some(d => d.id === item.id);
+          return (
+            <DrugItem
+              name={item.name}
+              onPress={() => navigation.navigate('Drug Details', {drug: item})}
+              isLearning={isLearning}
+            />
+          );
+        }}
+      />   
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1
+    flex: 1,
     padding: 10,
   },
   title:{
