6b90ed3bae26c9032efdb7c421b8a62ee506e449
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-05-09 18:55:59 +1000

    Milestone 2: Updated it to accept isLearning prop for graying out the drugs

--- src/components/DrugItem.js
+++ src/components/DrugItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function DrugItem({ name, onPress }){
+export default function DrugItem({ name, onPress, isLearning }){
   return(
-    <TouchableOpacity style {styles.button} onPress={onPress}>
+    <TouchableOpacity style={[styles.button, isLearning && styles.learningButton]} 
+    onPress={onPress}>
       <Text style={styles.text}>{name}</Text>
     </TouchableOpacity>
   );
@@ -21,5 +22,8 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 16,
   },
+  learningButton: {
+    backgroundColor: '#a3a3a3',
+  }
 });
 
