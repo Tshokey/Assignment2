9fa0716ffd445f4a346948a868b5b90c65f64034
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-05-09 15:18:31 +1000

    Milestone 2: Implemented bottom tab navigation with badge and stack-based Drugs flow

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import React from "react";
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useSelector } from "react-redux";
 
 import CategoryScreen from './src/screens/CategoryScreen';
 import DrugInCategoryScreen from './src/screens/DrugInCategoryScreen';
 import DrugDetailScreen from './src/screens/DrugDetailScreen';
+import LearningList from "./src/screens/LearningList";
 
 const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
-export default function App() {
+function DrugStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName= "Categories">
+    <Stack.Navigator initialRouteName= "Categories">
         <Stack.Screen name = "Categories" component = {CategoryScreen} options={{title: 'Drugs', headerTitleAlign: 'left',}}/>
         <Stack.Screen name =  "Drug List" component = {DrugInCategoryScreen} options={{headerTitle: '', headerBackTitle: 'Drugs in Category',}}/>
         <Stack.Screen name = "Drug Details" component = {DrugDetailScreen} options = {{headerTitle: '',headerBackTitle: 'Drug Details'}}/>
-        </Stack.Navigator>
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  const currentCount = useSelector(state => state.learning.current.length);
+
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen 
+        name ="Drugs" 
+        component={DrugStack}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Ionicons name="medkit" size={size} color={color} />
+          ),
+        }} />
+        <Tabs.Screen 
+        name ="Learning" 
+        component={LearningList} 
+        options={{
+          tabBarIcon: ({ size, color}) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+          tabBarBadge: currentCount > 0 ? currentCount : null,
+        }}/>
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
